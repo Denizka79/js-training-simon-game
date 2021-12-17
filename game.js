3ff8944725efafbdb124cbaab3f22fd40f53e793
Мещеряков Денис Владимирович <Meshcheryakov.DV@ruspace.digital>
2021-12-17 17:09:33 +0300

    Create arrays and button click flashes

--- game.js
+++ game.js
@@ -1,8 +1,31 @@
-//alert("Hi!");
-
 var randomNumber = 0;
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var numberOfButtons = document.querySelectorAll(".btn").length;
 
 function nextSequence() {
-    var someNumber = Math.floor(Math.random() * 4);
-    console.log(someNumber);
+    randomNumber = Math.floor(Math.random() * 4);
+    return randomNumber
+}
+
+var randomChosenColour = buttonColours[nextSequence()];
+
+var selectedButton = document.querySelector("." + randomChosenColour);
+
+for (var i = 0; i < numberOfButtons; i++) {
+    document.querySelectorAll(".btn")[i].addEventListener("click", function() {
+        var clickedButton = this;
+        var clickedButtonID = this.id;
+        //$(clickedButton).fadeOut(100).fadeIn(100);
+        var soundPath = "sounds/" + clickedButtonID + ".mp3";
+        var randomSound = new Audio(soundPath);
+        randomSound.play();
+        clickedButton.classList.add("pressed");
+        setTimeout(function() {
+            clickedButton.classList.remove("pressed");
+        }, 100);
+        userClickedPattern.push(clickedButtonID);
+        console.log(userClickedPattern);
+    });
 }
