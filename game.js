ee504c9a3eaca1b52ec9f5d9c7a6cb13851fafc6
Мещеряков Денис Владимирович <Meshcheryakov.DV@ruspace.digital>
2021-12-17 19:13:52 +0300

    Add sequence checking

--- game.js
+++ game.js
@@ -2,6 +2,9 @@ var randomNumber = 0;
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var keyPressed = false;
+var level = 1;
+var sequencesMatch = false;
 var numberOfButtons = document.querySelectorAll(".btn").length;
 
 function nextSequence() {
@@ -9,15 +12,22 @@ function nextSequence() {
     return randomNumber
 }
 
-var randomChosenColour = buttonColours[nextSequence()];
-
-var selectedButton = document.querySelector("." + randomChosenColour);
+document.addEventListener("keydown", function(event) {
+    keyPressed = true;
+    document.querySelector("h1").innerHTML = "Level " + level;
+    var randomChosenColour = buttonColours[nextSequence()];
+    gamePattern.push(randomChosenColour);
+    var randomButton = document.querySelector("." + randomChosenColour);
+    $(randomButton).fadeOut(100).fadeIn(100);
+    var randomButtonsoundPath = "sounds/" + randomChosenColour + ".mp3";
+    var randomButtonSound = new Audio(randomButtonsoundPath);
+    randomButtonSound.play();
+});
 
 for (var i = 0; i < numberOfButtons; i++) {
     document.querySelectorAll(".btn")[i].addEventListener("click", function() {
         var clickedButton = this;
         var clickedButtonID = this.id;
-        //$(clickedButton).fadeOut(100).fadeIn(100);
         var soundPath = "sounds/" + clickedButtonID + ".mp3";
         var randomSound = new Audio(soundPath);
         randomSound.play();
@@ -25,7 +35,22 @@ for (var i = 0; i < numberOfButtons; i++) {
         setTimeout(function() {
             clickedButton.classList.remove("pressed");
         }, 100);
-        userClickedPattern.push(clickedButtonID);
-        console.log(userClickedPattern);
+        if (keyPressed) {
+            userClickedPattern.push(clickedButtonID);
+            if (gamePattern.length === userClickedPattern.length) {
+                for (var j = 0; j < gamePattern.length; j++) {
+                    if (gamePattern[j] != userClickedPattern[j]) {
+                        sequencesMatch = false;
+                        document.querySelector("h1").innerHTML = "You lost!";
+                    } else {
+                        sequencesMatch = true;
+                    }
+                }
+            }
+            if (sequencesMatch = true) {
+                level = level + 1;
+                document.querySelector("h1").innerHTML = "Level " + level;
+            }
+        }
     });
 }
