8269d392eb643acb05f1d5eb4d1e7d37bd0f0ee8
Denis Meshcheryakov <denismeshcheryakov@MacBook-Air-Denis.local>
2021-12-18 20:27:19 +0300

    Finish 90%

--- game.js
+++ game.js
@@ -5,16 +5,15 @@ var userClickedPattern = [];
 var keyPressed = false;
 var level = 1;
 var sequencesMatch = false;
+var clicksCounter = 0;
 var numberOfButtons = document.querySelectorAll(".btn").length;
 
 function nextSequence() {
     randomNumber = Math.floor(Math.random() * 4);
     return randomNumber
 }
-
-document.addEventListener("keydown", function(event) {
-    keyPressed = true;
-    document.querySelector("h1").innerHTML = "Level " + level;
+function randomButtonFlash(lev) {
+    document.querySelector("h1").innerHTML = "Level " + lev;
     var randomChosenColour = buttonColours[nextSequence()];
     gamePattern.push(randomChosenColour);
     var randomButton = document.querySelector("." + randomChosenColour);
@@ -22,34 +21,54 @@ document.addEventListener("keydown", function(event) {
     var randomButtonsoundPath = "sounds/" + randomChosenColour + ".mp3";
     var randomButtonSound = new Audio(randomButtonsoundPath);
     randomButtonSound.play();
+}
+
+document.addEventListener("keydown", function(event) {
+    if (!keyPressed) {
+        keyPressed = true;
+        clicksCounter = 0;
+        randomButtonFlash(level);
+    }
 });
 
 for (var i = 0; i < numberOfButtons; i++) {
     document.querySelectorAll(".btn")[i].addEventListener("click", function() {
+        clicksCounter = clicksCounter + 1;
+        console.log(clicksCounter);
         var clickedButton = this;
         var clickedButtonID = this.id;
         var soundPath = "sounds/" + clickedButtonID + ".mp3";
         var randomSound = new Audio(soundPath);
         randomSound.play();
+        userClickedPattern.push(clickedButtonID);
         clickedButton.classList.add("pressed");
         setTimeout(function() {
             clickedButton.classList.remove("pressed");
         }, 100);
-        if (keyPressed) {
-            userClickedPattern.push(clickedButtonID);
+        if (keyPressed && clicksCounter === gamePattern.length) {
             if (gamePattern.length === userClickedPattern.length) {
+                console.log(gamePattern);
                 for (var j = 0; j < gamePattern.length; j++) {
                     if (gamePattern[j] != userClickedPattern[j]) {
+                        console.log(gamePattern[j] + " does not match " + userClickedPattern[j]);
+                        document.querySelector("h1").innerHTML = "You lost! Press a key to restart.";
                         sequencesMatch = false;
-                        document.querySelector("h1").innerHTML = "You lost!";
+                        gamePattern = [];
+                        userClickedPattern = [];
+                        level = 1;
+                        keyPressed = false;
                     } else {
                         sequencesMatch = true;
                     }
                 }
             }
-            if (sequencesMatch = true) {
+            if (sequencesMatch) {
                 level = level + 1;
-                document.querySelector("h1").innerHTML = "Level " + level;
+                setTimeout(function() {
+                    randomButtonFlash(level);
+                    userClickedPattern = [];
+                    clicksCounter = 0;
+                }, 1000);
             }
         }
     });
